8e037cda4bc4658f1e376e4a62479e19d9ce184a
DanielSchloms <e11701253@student.tuwien.ac.at>
2021-05-07 15:13:34 +0200

    added base cpp file

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+class FIFO {
+private:
+    int private_int;
+
+public:             
+    
+    int public_int;
+    
+    int public_function(){
+        return 1;
+    }
+
+};
+
+int main(int argc, char **argv) {
+    return 0;
+}
