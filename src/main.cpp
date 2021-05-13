44bbcdbaaf8ee5f9e00259a1c8610133230659c3
KonstantinRoehrl <e11777779@student.tuwien.ac.at>
2021-05-13 15:20:19 +0200

    Included OPENMP threads.

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,18 @@
 #include <thread>
 #include <vector>
 #include <chrono>
+#include <omp.h>
 #include "LockQueue.h"
 #include "LockQueue.h"
 #include "main.h"
 
-void test_enqueue(LockQueue * q, int id, int elements){
+void test_enqueue(LockQueue * q, int id, int enq_cnt){
     // Thread ID
     // std::thread::id my_id = std::this_thread::get_id();
     // Don't use actual thread ID, just use the thread's index
     int my_id = id;
 
-    for (int i = 0; i < elements; i++){
+    for (int i = 0; i < enq_cnt; i++){
         bool success = q->enq(i);
         if (success){
             if(!BENCHMARK){
@@ -27,13 +28,13 @@ void test_enqueue(LockQueue * q, int id, int elements){
     }
 }
 
-void test_dequeue(LockQueue * q, int id, int elements){
+void test_dequeue(LockQueue * q, int id, int deq_cnt){
     // Thread ID
     // std::thread::id my_id = std::this_thread::get_id();
     // Don't use actual thread ID, just use the thread's index
     int my_id = id;
 
-    for (int i = 0; i < elements; i ++){
+    for (int i = 0; i < deq_cnt; i ++){
         int error_code;
         int ret = q->deq(&error_code);
         if (ret < 0){
@@ -89,8 +90,8 @@ int main(int argc, char **argv){
     
  
     int num_threads = DEFAULT_THREADS;
-    int enq_cnt = 100000;   //Enq_cnt per thread
-    int deq_cnt = 100000;   //Deq_cnt per thread
+    int enq_cnt = 2;   //Enq_cnt per thread
+    int deq_cnt = 2;   //Deq_cnt per thread
 
     if (argc > 1){
         int arg_num_threads = atoi(argv[1]);
@@ -108,32 +109,58 @@ int main(int argc, char **argv){
     LockQueue q = LockQueue(enq_cnt*num_threads);
     if(!BENCHMARK){//
         std::cout << "Created Queue\n";
-    }
+    }   
+    
     std::vector<std::thread> threads;
 
     // Start time measurement
     auto start = std::chrono::system_clock::now();
 
     // Start enqueue threads
-    for(int i = 0; i < num_threads; i++){
-        threads.push_back(std::thread(test_enqueue, &q, i, enq_cnt));
+    
+    if(!USE_OPENMP){
+        for(int i = 0; i < num_threads; i++){
+            threads.push_back(std::thread(test_enqueue, &q, i, enq_cnt));
+        }
+    }
+    else{
+        #pragma omp parallel 
+        {
+            int id = omp_get_thread_num();
+            test_enqueue(&q, id, enq_cnt);
+        }
     }
 
     // Start dequeue threads
-    for(int i = 0; i < num_threads; i++){
-        threads.push_back(std::thread(test_dequeue, &q, i, deq_cnt));
+    
+    if(!USE_OPENMP){
+        for(int i = 0; i < num_threads; i++){
+            threads.push_back(std::thread(test_dequeue, &q, i, deq_cnt));
+        }
+    }
+    else{
+        #pragma omp parallel 
+        {
+            int id = omp_get_thread_num();
+            test_dequeue(&q, id, enq_cnt);
+        }
     }
 
-    // Join threads
-    for(auto& thread : threads){
-        if (thread.joinable()){
-            thread.join();
+
+    if(!USE_OPENMP){
+        // Join threads
+        if(!BENCHMARK){
+            std::cout << "Join Threads after Enqueue Test\n";
+        }
+        for(auto& thread : threads){
+            if (thread.joinable()){
+                thread.join();
+            }
         }
     }
     
-    if(!BENCHMARK){
-        std::cout << "Join Threads after Enqueue Test\n";
-    }
+    
+    
     // End timer
     auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now()-start).count();
     if(!BENCHMARK){
